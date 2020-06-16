8771a108b87300ae6c8a07a9ad12b6534fffa9ad
nathan <nathannash11@gmail.com>
2020-06-16 01:13:47 +0100

    added post work sliders

--- screens/FeelingsScreen.js
+++ screens/FeelingsScreen.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {StyleSheet, Text, View, Button} from 'react-native';
+import {StyleSheet, View, Button} from 'react-native';
 
 import FeelingSlider from '../components/FeelingSlider';
 
@@ -7,13 +7,14 @@ export default function FeelingsScreen({navigation}) {
 
     const onDone = () => {
         //todo get the values from the feeling sliders
-        //todo send the values to a server and get a value back
+        //todo send the values to the database
+        //todo using the feelings and the post work values send a request to the recommendation server
+        //todo display a loading animation
         //todo navigate to a recommendation or back to the distraction screen depending on the server response
 
         //navigation.navigate('DistractionsScreen');
         navigation.navigate('RecommendationScreen');
     };
-
 
     return (
         <View style={styles.container}>
@@ -35,7 +36,6 @@ export default function FeelingsScreen({navigation}) {
             />
             <Button
                 title={'Done'}
-                style={styles.doneButton}
                 onPress={onDone}
             />
         </View>
@@ -46,7 +46,4 @@ const styles = StyleSheet.create({
     container: {
         flex: 1,
     },
-
-    doneButton: {}
-
 });
--- screens/PostWorkScreen.js
+++ screens/PostWorkScreen.js
@@ -1,14 +1,43 @@
 import React from 'react';
-import {StyleSheet, Text, View} from 'react-native';
+import {Button, StyleSheet, Text, View} from 'react-native';
+import FeelingSlider from "../components/FeelingSlider";
 
-export default function FeelingsScreen() {
+export default function FeelingsScreen({navigation}) {
+    const onDone = () => {
+        //todo get the values from the feeling sliders
+        //todo send the values to the database
+
+        navigation.navigate('FeelingsScreen');
+    };
+
     return (
-        <View>
-            <Text>Post work</Text>
-        </View>
+            <View style={styles.container}>
+                <FeelingSlider
+                    wordsArr={['wat', 'nah', 'kinda', 'yep', 'in the zone']}
+                    sliderName={'Focus...'}
+                />
+                <FeelingSlider
+                    wordsArr={['nah mate', 'nah', 'give or take', 'unreal', 'godly']}
+                    sliderName={'Productive...'}
+                />
+                <FeelingSlider
+                    wordsArr={['no chance', 'nah', 'not really', 'oh yeah', 'on the gram rn']}
+                    sliderName={'Distracted...'}
+                />
+                <FeelingSlider
+                    wordsArr={['not even remotely', 'nah', 'kinda', 'yes sir', 'like great river']}
+                    sliderName={'Flow...'}
+                />
+                <Button
+                    title={'Done'}
+                    onPress={onDone}
+                />
+            </View>
     );
 }
 
 const styles = StyleSheet.create({
-
+    container: {
+        flex: 1,
+    },
 });
